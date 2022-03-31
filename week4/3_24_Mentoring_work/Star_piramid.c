915b0f2375d82af0835675ad1509971eed4a08b0
22-JWL <jck1004ee@gmail.com>
2022-03-31 15:27:20 +0900

    t

--- week4/3_24_Mentoring_work/Star_piramid.c
+++ week4/3_24_Mentoring_work/Star_piramid.c
@@ -9,7 +9,7 @@ int main (void)
     //     }
     //     printf("\n");
     // }
-
+//--
     // for(int i=5; i>0; i--)
     // {
     //     for(int j = 0; j<i; j++){
@@ -17,16 +17,16 @@ int main (void)
     //     }
     //     printf("\n");
     // }
-
-    for(int i = 0; i<6; i++)
-    {
-        for(int j = 0; j<i; j++){
-            for(int k = 5; j<k; k--){
-                printf(" ");
-            }
-            printf("*\n");
-        }
-    }
+//--
+    // for(int i = 0; i<6; i++)
+    // {
+    //     for(int j = 0; j<i; j++){
+    //         for(int k = 5; j<k; k--){
+    //             printf(" ");
+    //         }
+    //         printf("*\n");
+    //     }
+    // }
     
     return 0 ;
 
---
+++ week4/3_24_Mentoring_work/tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+ for(int i=5; i>0; i--)
+    {
+        for(int j = 0; j<i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
